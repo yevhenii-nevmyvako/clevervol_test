e856e771e95dd3e98b744c7f0712813bd9f73c8e
Yevhenii <yevhenii.nevmyvako@gmail.com>
2024-06-18 18:50:36 +0300

    Add const.py & cli interface in crawler.py

---
+++ crawler.py
@@ -0,0 +1,16 @@
+import click
+
+from core import crawler
+
+
+@click.command()
+@click.argument('url', type=str)
+@click.argument('dst_filepath', default='google_ads_data.csv')
+@click.option('--limit', '-l', default=10, help='Limit for page crawling.')
+def crawler_cli(url: str, limit: int, dst_filepath: str) -> None:
+    """Command line interface for crawler URL and saves processed data & summary to DST_FILEPATH in csv format"""
+    crawler(url, limit, dst_filepath)
+
+
+if __name__ == "__main__":
+    crawler_cli()
